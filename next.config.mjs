284ba78d081f869bf9ee3b36b624a98f439b567c
yossafi <yossef0110215@gmail.com>
2024-10-05 02:36:19 +0300

    maybe the friends.

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === "development" ? "/":"/aouplr",
+    basePath: process.env.NODE_ENV === "development" ? "":"/aouplr",
     reactStrictMode: true,
     swcMinify: true,
     compiler: {
