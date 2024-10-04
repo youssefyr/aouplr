637edd1ac3364d0f7d01ab0545b7d7e024baab2b
yossafi <yossef0110215@gmail.com>
2024-10-05 02:43:03 +0300

    maybe the maybes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,4 @@
 import withPWA from 'next-pwa';
-import fs from 'fs';
-import path from 'path';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,20 +8,6 @@ const nextConfig = {
     compiler: {
         removeConsole: process.env.NODE_ENV !== "development"
     },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            // Ensure the data folder is included in the build
-            const dataPath = path.resolve(process.cwd(), 'public/data');
-            if (fs.existsSync(dataPath)) {
-                config.module.rules.push({
-                    test: /\.(json)$/,
-                    include: [dataPath],
-                    type: 'asset/resource',
-                });
-            }
-        }
-        return config;
-    }
 };
 
 export default withPWA({
