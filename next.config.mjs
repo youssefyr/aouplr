c77dbd9e029fee43b468cff056508cb3bceb34f7
yossafi <yossef0110215@gmail.com>
2024-10-05 03:07:22 +0300

    smalldeplyfix6

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@ import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.NODE_ENV === "development" ? "":"",  // <=== enables static exports
+    basePath: process.env.NODE_ENV === "development" ? "":"/aouplr",  // <=== enables static exports
     reactStrictMode: true,
     swcMinify: true,
     compiler: {
